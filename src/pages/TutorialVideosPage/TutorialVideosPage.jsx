a1f3f700e78bb32f58b57d0fafb83c79fe59770e
Sodiya Tobiloba <75803621+Tobi153@users.noreply.github.com>
2025-05-03 13:15:55 +0100

    LFC Intranet Page v1.0

--- src/pages/TutorialVideosPage/TutorialVideosPage.jsx
+++ src/pages/TutorialVideosPage/TutorialVideosPage.jsx
@@ -1,23 +1,38 @@
 import { useParams } from "react-router-dom";
 import "./TutorialVideosPage.css";
 import { watchTutorials } from "../../services/api/data";
+import { TutorialCard } from "../../components/ui/TutorialCard/TutorialCard";
 
 export function TutorialVideosPage() {
   const { id } = useParams();
   const tutorial = watchTutorials.find((t) => t.id === id);
-
+  const relatedVideos = watchTutorials.filter((t) => t.id !== id);
   return (
-    <div className="tutorial-video-container">
-      <iframe
-        width="560"
-        height="315"
-        className="w-full h-[600px] rounded-2xl"
-        src={`https://www.youtube.com/embed/${tutorial.id}?si=lTG9LEur9T_ypR-c`}
-        title={tutorial.title}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
+    <div className="tutorial-video-section">
+      <div className="tutorial-video-container">
+        <iframe
+          width="560"
+          height="315"
+          className="w-8/10 h-[500px] rounded-2xl"
+          src={`https://www.youtube.com/embed/${tutorial.id}?si=lTG9LEur9T_ypR-c`}
+          title={tutorial.title}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <div className="related-videos-container w-8/10 mt-7 ">
+        <h1 className="related-videos-heading mb-5 text-2xl font-semibold text-center text-gray-900 ">
+          Related videos
+        </h1>
+
+        <div className="related-videos h-[220px] grid grid-cols-3 gap-10 grid-rows-[auto_1fr] overflow-y-scroll">
+          {relatedVideos.map((tutorial, index) => (
+            <TutorialCard key={index} tutorial={tutorial} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
